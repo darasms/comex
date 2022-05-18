f588abbf121bb0fa13a673c385053e33884ba5bf
Dara S. Mariano <darasouza@ciandt.com>
2022-05-18 15:28:18 -0300

    Remocao de sujeira de variavel

--- src/main/java/br/com/alura/comex/ProcessadorDeCsv.java
+++ src/main/java/br/com/alura/comex/ProcessadorDeCsv.java
@@ -18,7 +18,7 @@ public class ProcessadorDeCsv {
 
         try {
             URL recursoCSV = ClassLoader.getSystemResource(arq_name);
-            Path caminhoDoArquivo = caminhoDoArquivo = Path.of(recursoCSV.toURI());
+            Path caminhoDoArquivo = Path.of(recursoCSV.toURI());
 
             Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo);
 
