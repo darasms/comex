d6fd1493cfa775f2d1eea3672e5fb15e391142c9
Dara S. Mariano <darasouza@ciandt.com>
2022-06-22 20:22:33 -0300

    [TESTE] - Teste relatorio pedidos categoria correto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,35 +1,26 @@
 INSERT INTO categorias(nome, status) VALUES ('INFORMÁTICA', 'ATIVA');
-INSERT INTO categorias(nome, status) VALUES ('LIVROS', 'ATIVA');
-INSERT INTO categorias(nome, status) VALUES ('FILMES', 'ATIVA');
-INSERT INTO categorias(nome, status) VALUES ('MODA', 'INATIVA');
 
-INSERT INTO produtos(nome, descricao, preco_unitario, quantidade_estoque, categoria_id ) VALUES ("Mouse", "Mouse", 30, 3, 1);
+INSERT INTO categorias(nome, status) VALUES ('FILMES', 'ATIVA');
+
+INSERT INTO produtos(nome, descricao, preco_unitario, quantidade_estoque, categoria_id ) VALUES ("Mouse", "Mouse", 30.50, 15, 34);
+
+INSERT INTO produtos(nome, descricao, preco_unitario, quantidade_estoque, categoria_id ) VALUES ("Moana", "BlueRay", 25, 08, 33);
+
 
 INSERT INTO clientes(cpf, bairro, cidade, complemento, estado, numero, rua, nome, telefone)
 VALUES (154637485766, "Janta Genebra", "Campinas", "H22", "SP", 366, "Rua da esquina", "Gabriel", "199283747474");
 
-INSERT INTO clientes(cpf, bairro, cidade, complemento, estado, numero, rua, nome, telefone)
-VALUES (154637485766, "Janta Genebra", "Campinas", "G06", "SP", 366, "Rua da esquina", "Maria", "190083747474");
 
+INSERT INTO pedidos(data, desconto, tipo_desconto, cliente_id) VALUES ("2022/05/30", "0.00", "NENHUM", 17);
+INSERT INTO pedidos(data, desconto, tipo_desconto, cliente_id) VALUES ("2022/05/30", "0.00", "NENHUM", 17);
+INSERT INTO pedidos(data, desconto, tipo_desconto, cliente_id) VALUES ("2022/05/30", "0.00", "NENHUM", 17);
 
-INSERT INTO pedidos(data, desconto, tipo_desconto, cliente_id) VALUES ("2022/05/30", "0.00", "NENHUM", 1);
 
 INSERT INTO itens_pedido(desconto, preco_unitario, quantidade, tipo_desconto, pedido_id, produto_id)
-VALUES (0.00, 223.6, 3, "NENHUM", 1, 1);
+VALUES (0.00, 30.5, 7, "NENHUM", 39, 33);
 
+INSERT INTO itens_pedido(desconto, preco_unitario, quantidade, tipo_desconto, pedido_id, produto_id)
+VALUES (0.00, 30.5, 3, "NENHUM", 40, 33);
 
-SELECT c.nome, SUM(item.quantidade) AS quantidade, SUM((item.preco_unitario * item.quantidade)) AS montanteVendido
-FROM itens_pedido item
-JOIN produtos p
-JOIN categorias c
-WHERE item.produto_id = p.id AND p.categoria_id = c.id
-
-
-
-
-
-INSERT INTO usuarios(email, senha, cliente_id) VALUES("aluno@exemplo.com", "$2a$10$JhY8lcscK7wotSZJCnNCL..ZmEq.R9TUGPo00Bai1qc4GkczudRTW", 4);
-
-
-
-
+INSERT INTO itens_pedido(desconto, preco_unitario, quantidade, tipo_desconto, pedido_id, produto_id)
+VALUES (0.00, 25, 3, "NENHUM", 41, 34);
--- src/test/java/br/com/alura/comex/repository/PedidoRepositoryTest.java
+++ src/test/java/br/com/alura/comex/repository/PedidoRepositoryTest.java
@@ -1,6 +1,5 @@
 package br.com.alura.comex.repository;
 
-import br.com.alura.comex.model.*;
 import br.com.alura.comex.model.projecao.RelatorioPedidosPorCategoriaProjecao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
-import javax.persistence.Tuple;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,8 +28,6 @@ public class PedidoRepositoryTest {
     @Test
     public void deveriaRetornarUmRegistroParaCadaCategoriaComDescontoQuantidadeEmUmDosRegistros() {
 
-        //persistirRegistrosTeste();
-
         List<RelatorioPedidosPorCategoriaProjecao> resultado = repository.findPedidosPorCategoria();
 
         System.out.println(resultado.get(0).getNome());
@@ -45,49 +40,6 @@ public class PedidoRepositoryTest {
                         RelatorioPedidosPorCategoriaProjecao::getQuantidadeProdutos,
                         RelatorioPedidosPorCategoriaProjecao::getMontanteVendido)
                 .containsExactly(tuple(("INFORMÁTICA"), (10L), new BigDecimal("305.00")), tuple("FILMES", 3L, new BigDecimal("75.00")) );
-    }
-
-    private void persistirRegistrosTeste() {
-
-        Categoria informatica = new Categoria("INFORMÁTICA");
-        Categoria filmes = new Categoria("FILMES");
-
-        Produto mouse = new Produto("Mouse", "Mouse", new BigDecimal("30.50"), 15, informatica);
-        Produto moana = new Produto("Moana", "BlueRay",  new BigDecimal(25), 8, filmes);
-
-        Endereco endereco = new Endereco("Rua da esquina", "366", "H22", "Santa Genebra", "Campinas", "SP");
-        Cliente kelvin = new Cliente("Kelvin", 4156667228L, "198273666444", endereco);
-
-        Pedido pedido1 = new Pedido(kelvin);
-        Pedido pedido2 = new Pedido(kelvin);
-        Pedido pedido3 = new Pedido(kelvin);
-
-        kelvin.adicionarPedido(pedido1);
-        kelvin.adicionarPedido(pedido2);
-        kelvin.adicionarPedido(pedido3);
-
-        ItemDePedido item1 = new ItemDePedido(7, mouse);
-        ItemDePedido item3 = new ItemDePedido(3, mouse);
-        ItemDePedido item2 = new ItemDePedido(3, moana);
-
-        em.persist(informatica);
-        em.persist(filmes);
-
-        em.persist(mouse);
-        em.persist(moana);
-
-        em.persist(kelvin);
-
-
-        em.persist(pedido1);
-        em.persist(pedido2);
-        em.persist(pedido3);
-
-        pedido1.adicionarItem(item1);
-        pedido2.adicionarItem(item2);
-        pedido3.adicionarItem(item3);
-
-
     }
 
 }
